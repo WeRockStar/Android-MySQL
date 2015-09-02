5687506cda4399c171a4cda6e8089461e2fee2cb
WeRockStar <vr_freestyle@outlook.com>
2015-09-03 02:50:45 +0700

    call async Add new product

--- app/src/main/java/com/example/kotchaphan/android_mysql/NewProductActivity.java
+++ app/src/main/java/com/example/kotchaphan/android_mysql/NewProductActivity.java
@@ -1,37 +1,64 @@
 package com.example.kotchaphan.android_mysql;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class NewProductActivity extends AppCompatActivity {
 
+    private ProgressDialog pDialog;
+
+    JSONParser jsonParser = new JSONParser();
+    EditText inputName;
+    EditText inputPrice;
+    EditText inputDesc;
+
+    //url
+    private static String url_create_product = "http://192.168.56.1/android_mysql/create_product.php";
+
+    private static final String TAG_SUCCESS = "success";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_product);
+
+        inputName = (EditText)findViewById(R.id.inputName);
+        inputPrice = (EditText)findViewById(R.id.inputPrice);
+        inputDesc = (EditText)findViewById(R.id.inputDesc);
+
+        Button btnCreateProducts = (Button)findViewById(R.id.btnCreateProduct);
+        btnCreateProducts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //call async
+                new CreateNewProduct().execute();
+            }
+        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_new_product, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    class CreateNewProduct extends AsyncTask<String , String , String>{
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
         }
 
-        return super.onOptionsItemSelected(item);
+        @Override
+        protected String doInBackground(String... params) {
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(String... values) {
+            super.onProgressUpdate(values);
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+        }
     }
 }
