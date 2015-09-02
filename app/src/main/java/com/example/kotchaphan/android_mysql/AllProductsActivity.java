84bc6792e88c86260980c83af8b17ff2ea0bf276
WeRockStar <vr_freestyle@outlook.com>
2015-09-03 00:58:57 +0700

    create edit products

--- app/src/main/java/com/example/kotchaphan/android_mysql/AllProductsActivity.java
+++ app/src/main/java/com/example/kotchaphan/android_mysql/AllProductsActivity.java
@@ -1,37 +1,86 @@
 package com.example.kotchaphan.android_mysql;
 
+import android.app.ListActivity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
 
-public class AllProductsActivity extends AppCompatActivity {
+import org.json.JSONArray;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class AllProductsActivity extends ListActivity {
+
+    private ProgressDialog pDialog;
+    //json object
+    JSONParser jParser = new JSONParser();
+    ArrayList<HashMap<String, String>> productsList;
+    //url
+    private static String url_all_products = "http://localhost/android_mysql/get_all_products.php";
+
+    private static final String TAG_SUCCESS = "success";
+    private static final String TAG_PRODUCTS = "products";
+    private static final String TAG_PID = "pid";
+    private static final String TAG_NAME = "name";
+
+    //producs json arrsy
+    JSONArray product = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_products);
+
+        productsList = new ArrayList<HashMap<String, String>>();
+        new LoadAllProcuts().execute();
+
+        //get list view
+        ListView lv = getListView();
+
+        //on select
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //get pid from edit text
+                String pid = ((TextView)view.findViewById(R.id.pid)).getText().toString();
+
+                //Start new intent
+                Intent intent = new Intent(getApplicationContext() , EditProductActivity.class);
+                //send pid to Edit Activity
+                intent.putExtra(TAG_PID , pid);
+
+            }
+        });
+    }
+}
+
+class LoadAllProcuts extends AsyncTask<String, String, String> {
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_all_products, menu);
-        return true;
+    protected String doInBackground(String... params) {
+        return null;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    protected void onProgressUpdate(String... values) {
+        super.onProgressUpdate(values);
+    }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
     }
 }
